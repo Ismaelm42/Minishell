29143bcdc39e892acc2812de71fcab19ce5432fe
Ismaelm42 <115977088+Ismaelm42@users.noreply.github.com>
2023-08-21 22:06:39 +0200

    lexer maker con comillas sin espacios ok

--- src/parser/get_lexer/lexer_maker.c
+++ src/parser/get_lexer/lexer_maker.c
@@ -1,6 +1,5 @@
 #include "../../../include/minishell.h"
 
-
 /*
 Esta función se encarga de gestionar las comillas simples y dobles.
 */
@@ -10,10 +9,25 @@ void	quoted_lexer_splitter(int *n, char **s, char ***lexer)
 	int		length;
 
 	length = 0;
-	c = **s;
-	while ((*s)[length + 1] != c && (*s)[length + 1] != '\0')
-		length++;
-	length += 2;
+	while (1)
+	{
+		c = 0;
+		if ((*s)[length] == '\'' || (*s)[length] == '\"')
+		{
+			c = (*s)[length++];
+			while ((*s)[length] != c && (*s)[length] != '\0')
+				length++;
+			length++;
+		}
+		while ((*s)[length] != '\0' && (*s)[length] != ' '
+			&& (*s)[length] != '\t' && (*s)[length] != '|'
+			&& (*s)[length] != '<' && (*s)[length] != '>')
+			length++;
+		if ((*s)[length] == '\0' || (*s)[length] == ' '
+			|| (*s)[length] == '\t' || (*s)[length] == '|'
+			|| (*s)[length] == '<' || (*s)[length] == '>')
+			break ;
+	}
 	(*lexer)[*n] = ft_substr(*s, 0, length, 0);
 	*n += 1;
 	while (length -- > 0)
@@ -46,14 +60,30 @@ no coincida ningún caracter con los caracteres especiales.
 */
 void	words_splitter(int *n, char **s, char ***lexer)
 {
-	int	length;
+	char	c;
+	int		length;
 
 	length = 0;
-	while ((*s)[length] != '\'' && (*s)[length] != '"'
-		&& (*s)[length] != '<' && (*s)[length] != '>'
-		&& (*s)[length] != ' ' && (*s)[length] != '\t'
-		&& (*s)[length] != '|' && (*s)[length] != '\0')
-	length++;
+	while (1)
+	{
+		while ((*s)[length] != '\0' && (*s)[length] != ' '
+			&& (*s)[length] != '\t' && (*s)[length] != '|'
+			&& (*s)[length] != '\'' && (*s)[length] != '\"'
+			&& (*s)[length] != '<' && (*s)[length] != '>')
+			length++;
+		c = 0;
+		if ((*s)[length] == '\'' || (*s)[length] == '\"')
+		{
+			c = (*s)[length++];
+			while ((*s)[length] != c && (*s)[length] != '\0')
+				length++;
+			length++;
+		}
+		if ((*s)[length] == '\0' || (*s)[length] == ' '
+			|| (*s)[length] == '\t' || (*s)[length] == '|'
+			|| (*s)[length] == '<' || (*s)[length] == '>')
+			break ;
+	}
 	(*lexer)[*n] = ft_substr(*s, 0, length, 0);
 	*n += 1;
 	while (length -- > 0)
@@ -71,14 +101,15 @@ char	**lexer_maker(char *s)
 
 	n = 0;
 	size = lexer_counter(s);
+	printf("size = %d\n\n", size);
 	lexer = (char **)calloc(sizeof(char **), size + 1);
 	while (n < size)
 	{
 		while (*s == ' ' || *s == '\t')
 			s++;
-		if (*s == '\'' || *s == '"')
-			quoted_lexer_splitter(&n, &s, &lexer);
-		else if (*s == '<' || *s == '>' || *s == '|')
+		// if (*s == '\'' || *s == '"')
+		// 	quoted_lexer_splitter(&n, &s, &lexer);
+		if (*s == '<' || *s == '>' || *s == '|')
 			redirection_lexer_splitter(&n, &s, &lexer);
 		else
 			words_splitter(&n, &s, &lexer);
